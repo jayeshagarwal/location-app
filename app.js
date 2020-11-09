944ca691804fb16fd850e54af80c35a4772ff092
jayeshagarwal <jayeshagarwal58@gmail.com>
2020-11-09 18:17:08 +0530

    initial commits

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const unirest = require("unirest");
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    const apiCall = unirest(
+        "GET",
+        "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
+    );
+     
+    apiCall.headers({
+        "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+        "x-rapidapi-key": "0d3883ce61msh33099b70a211636p177184jsn61c3afe16020"     
+    });
+     
+    apiCall.end(function(result) {
+        if (res.error) throw new Error(result.error);
+        console.log(result.body);
+        res.send(result.body);
+     
+    });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
